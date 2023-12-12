7de7a10ea2539556a0a22b86597b3a2e3cfd6694
Quentin Benoit <quentin60690@hotmail.fr>
2023-12-12 14:36:47 +0100

    V3 Mise en page

--- prets.js
+++ prets.js
@@ -1,5 +1,19 @@
 var prets = new Map();
 
 prets.set("1", {numeroAdh: "01", codeExp: "C01"});
+prets.set("2", {numeroAdh: "05", codeExp: "C20"});
+prets.set("3", {numeroAdh: "03", codeExp: "C09"});
+prets.set("4", {numeroAdh: "08", codeExp: "C50"});
+prets.set("5", {numeroAdh: "66", codeExp: "C07"});
+prets.set("6", {numeroAdh: "15", codeExp: "C10"});
+prets.set("7", {numeroAdh: "50", codeExp: "C35"});
+prets.set("8", {numeroAdh: "02", codeExp: "C02"});
+prets.set("9", {numeroAdh: "99", codeExp: "C100"});
+prets.set("10", {numeroAdh: "85", codeExp: "C60"});
+prets.set("11", {numeroAdh: "40", codeExp: "C47"});
+prets.set("12", {numeroAdh: "13", codeExp: "C25"});
+prets.set("13", {numeroAdh: "09", codeExp: "C90"});
+prets.set("14", {numeroAdh: "110", codeExp: "C150"});
+prets.set("15", {numeroAdh: "1050", codeExp: "C1287"});
 
 console.log(prets.get('1'));

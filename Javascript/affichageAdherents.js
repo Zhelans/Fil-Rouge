f4e76d338f19981c37d53908153f5900e1807d5b
Groarmon <groarmon@gmail.com>
2023-12-20 23:54:14 +0100

    Commentaires

--- Javascript/affichageAdherents.js
+++ Javascript/affichageAdherents.js
@@ -1,39 +1,69 @@
+//Appel DOM
 var listeAdherents = document.getElementById("liste");
 var boutonPrecedent = document.getElementById("boutonPrecedent");
 var boutonSuivant = document.getElementById("boutonSuivant");
 
-
+//Variable dont la valeur est le point de départ de l'affichage du tableau.
 var portionTableau = 0;
 
-
+//Initialisation de l'affichage
 window.onload = function(){affichageListeAdherents(portionTableau)};
 
-
+//Evénements
 boutonPrecedent.onclick = dixPrecedents;
 boutonSuivant.onclick = dixSuivant;
 
+
+
+
+//--------------------------FONCTIONS----------------------------//
+
+//---Fonctions des boutons précédents/suivants---//
+
+/**
+ * Affiche les 10 valeurs suivantes.
+ */
 function dixSuivant() {
+    //On ajoute 10 mais on peut mettre n'importe quel nombre.
     portionTableau +=10;
+    //Le max est la taille ou la longueur de ce qu'on veut naviguer.
     if (portionTableau > adherents.size){
+        //Une fois au max, on enlève le nombre qu'on vient d'ajouter pour ne pas pouvoir avancer.
         portionTableau -= 10;
     }
-    console.log(portionTableau);
-    listeAdherents.innerHTML= "";
-    affichageListeAdherents(portionTableau);
-}
-function dixPrecedents() {
-    portionTableau -=10;
-        if (portionTableau < 0){
-            portionTableau = 0;
-        }
-    console.log(portionTableau);
+    //réinitialise la liste et la rempli avec les nouvelles valeurs
     listeAdherents.innerHTML= "";
     affichageListeAdherents(portionTableau);
 }
 
+/**
+ * Affiche les 10 valeurs précédentes. 
+ */
+function dixPrecedents() {
+    //On enlève 10 mais on peut mettre n'importe quel nombre.
+    portionTableau -=10;
+    //Le minimum est toujours 0.
+    if (portionTableau < 0){
+        //On remet n'importe quel nombre négatif à zéro.
+        portionTableau = 0;
+    }
+    //réinitialise la liste et la rempli avec les nouvelles valeurs
+    listeAdherents.innerHTML= "";
+    affichageListeAdherents(portionTableau);
+}
+
+//---Fonctions qui affichent les données---//
+
+/**
+ * Affiche le contenu d'un tableau en remplissant chaque étage avec du HTML
+ * @param {number} indiceTableau Position dans un tableau 
+ */
 function affichageListeAdherents(indiceTableau) {
+    //On copie des valeurs de "adherents.js" dans un tableau.
     let adherentsArray = Array.from(adherents.values());
+    //On ne prend que 10 valeurs à la fois
     let dixAdherents = adherentsArray.slice(indiceTableau, indiceTableau+10);
+    //boucle qui rempli la liste en concaténant chaque étage.
     for (let valeur of dixAdherents) {
         listeAdherents.innerHTML +='<tr class="row-cols-auto">'+
                                         '<td class="col-1">'+adherentsNumero(valeur)+'</td>'+
@@ -46,19 +76,38 @@ function affichageListeAdherents(indiceTableau) {
     }
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne nom de l'adhérent de la clé valeur en paramètre
+ */
 function adherentsNom(i) {
     return i.nom;
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne prénom de l'adhérent de la clé valeur en paramètre
+ */
 function adherentsPrenom(i) {
     return i.prenom;
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne numéro de l'adhérent de la clé valeur en paramètre
+ */
 function adherentsNumero(i) {
     return i.numeroAdherent;
-
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne un O/X ou un icone.
+ */
 function adherentsCotisation(i) {
     let dateCotisation = new Date(i.dateCotisation);
     if (dateCotisation > dateAnDernier()) {
@@ -68,6 +117,11 @@ function adherentsCotisation(i) {
     }
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne un X ou une instance de prêt.
+ */
 function adherentsPret(i) {
     if (i.pret == null){
         return "X";
@@ -76,6 +130,11 @@ function adherentsPret(i) {
     }
 }
 
+/**
+ * 
+ * @param {number} valeur 
+ * @returns retourne un X ou un certains montant d'amende.
+ */
 function adherentsAmende(i) {
     if (i.amende == null){
         return "X";
@@ -84,7 +143,12 @@ function adherentsAmende(i) {
     }
 }
 
+/**
+ *  
+ * @returns la date exacte de l'an dernier
+ */
 function dateAnDernier() {
+    //prend deux fois la même temps, enlève un an à la deuxième.
     let aujourdhui = new Date();
     let anDernier = new Date();
     anDernier.setFullYear(aujourdhui.getFullYear() - 1);
--- Javascript/rechercheAdherents.js
+++ Javascript/rechercheAdherents.js
@@ -1,47 +1,96 @@
+//Appel DOM
 var barreRechercheAdherent = document.getElementById("barreRechercheAdherent");
 var boutonPrecedentRecherche = document.getElementById("boutonPrecedentRecherche");
 var boutonSuivantRecherche = document.getElementById("boutonSuivantRecherche");
 var messageErreurRecherche = document.getElementById("messageErreurRecherche");
+
+//Variable des résultats la recherche : un tableau vide.
 var resultatRecherche = [];
 
+//regex pour contrôler les caractères spéciaux.
 var regex = /[!@#$%^&*()[\]=,.;?":{}|<>]/g
 
+
+//Dès qu'on tape une chose dans la barre de recherche, ça déclenche une recherche.
 barreRechercheAdherent.oninput= function (){
+    //Pour être sûr que le regex teste bien à partir du premier terme de la chaine.
     regex.lastIndex = 0;
+    //Si le test est faux, on cherche.
     if  (regex.test(barreRechercheAdherent.value)==false){
         toggleBoutons();
         messageErreurRecherche.className = "d-none";
         rechercheAdherent();
+    //Si le test est vrai, on désactive la recherche.
     } else {
         disableBoutons();
         messageErreurRecherche.className = "text-danger";
     }
 }
 
+//Evénements des boutons.
 boutonPrecedentRecherche.onclick = dixPrecedentsRecherche;
 boutonSuivantRecherche.onclick = dixSuivantsRecherche;
 
 
+//--------------------------FONCTIONS----------------------------//
 
+//-----------Fonctions des boutons-----------//
 
+/**
+ * Affiche les 10 valeurs suivantes. Pour les résultats de la recherche.
+ */
 function dixSuivantsRecherche() {
+    //On ajoute 10 mais on peut mettre n'importe quel nombre.
     portionTableau +=10;
+     //Le max est la taille ou la longueur de ce qu'on veut naviguer.
     if (portionTableau > resultatRecherche.length){
+        //Une fois au max, on enlève le nombre qu'on vient d'ajouter pour ne pas pouvoir avancer.
         portionTableau -= 10;
     }
-    console.log(resultatRecherche);
+    //réinitialise la liste et la rempli avec les nouvelles valeurs
     listeAdherents.innerHTML= "";
     affichageResultatRecherche(resultatRecherche);
 }
+
+/**
+ * Affiche les 10 valeurs précédentes. Pour les résultats de la recherche.
+ */
 function dixPrecedentsRecherche() {
+    //On enlève 10 mais on peut mettre n'importe quel nombre.
     portionTableau -=10;
+        //Le minimum est toujours 0.
         if (portionTableau < 0){
+        //On remet n'importe quel nombre négatif à zéro.
             portionTableau = 0;
         }
-    console.log(resultatRecherche);
+    //Réinitialise la liste et la rempli avec les nouvelles valeurs.
     listeAdherents.innerHTML= "";
     affichageResultatRecherche(resultatRecherche);
 }
+
+/**
+ * Active et désactive les boutons recherche en fonction du test regex.
+ */
+function toggleBoutons() {
+    if (barreRechercheAdherent.value != ""){
+        boutonsListe.classList.add("d-none");
+        boutonsRecherche.classList.remove("d-none");
+    } else {
+        boutonsListe.classList.remove("d-none");
+        boutonsRecherche.classList.add("d-none");
+    }
+}
+
+/**
+ * Désactive les boutons recherche.
+ */
+function disableBoutons() {
+    boutonsListe.classList.remove("d-none");
+    boutonsRecherche.classList.add("d-none");
+}
+
+//-----------Fonctions de la recherche-----------//
+
 
 function rechercheAdherent() {
     portionTableau = 0;
@@ -56,12 +105,18 @@ function rechercheAdherent() {
     affichageResultatRecherche(resultatRecherche);
 }
 
+/**
+ * Affiche le contenu d'un tableau en remplissant chaque étage avec du HTML.
+ * @param {Array.<object>} tableauRecherche Nouveau tableau contenant des valeurs.
+ */
 function affichageResultatRecherche(tableauRecherche) {
-
     let html = "";
+    //On découpe de tableau des résultat
     let dixResultats = tableauRecherche.slice(portionTableau, portionTableau+10);
+    //boucle qui rempli la liste en concaténant chaque étage.
     for (let valeur of dixResultats) {
-        html += '<tr class="row-cols-auto">'+
+        //Au lieu de concaténer chaque étage à chaque boucle, on le rajoute à la valeur de "html".
+        html += '<tr class="row-cols-auto" onclick="window.open(\'http://127.0.0.1:5500/liste_adherents.html\')">'+
                     '<td class="col-1">'+adherentsNumero(valeur)+'</td>'+
                     '<td class="col">'+adherentsNom(valeur)+'</td>'+
                     '<td class="col d-none d-sm-table-cell">'+adherentsPrenom(valeur)+'</td>'+
@@ -70,23 +125,6 @@ function affichageResultatRecherche(tableauRecherche) {
                     '<td class="col-1">'+adherentsAmende(valeur)+'</td>'+
                 '</tr>';
     }
+    //Pour afficher plus que le dernier résultat.
     listeAdherents.innerHTML = html;
-}
-
-function toggleBoutons() {
-
-    if (barreRechercheAdherent.value != ""){
-        boutonsListe.classList.add("d-none");
-        boutonsRecherche.classList.remove("d-none");
-    } else {
-        console.log("poloppo");
-        boutonsListe.classList.remove("d-none");
-        boutonsRecherche.classList.add("d-none");
-    }
-}
-
-function disableBoutons() {
-    console.log("disabled");
-    boutonsListe.classList.remove("d-none");
-    boutonsRecherche.classList.add("d-none");
 }

ceae5600f87258ea5ae94bdab8e6806863c0e10c
Stephane Meunier <steph.m.15061997@gmail.com>
2023-12-22 14:41:42 +0100

    Tri BD

--- Javascript/recherche_BD.js
+++ Javascript/recherche_BD.js
@@ -1,80 +1,55 @@
-    // Recuperation du HTML
-    var triAut = document.getElementById ('triAuteur');
-    var triSer = document.getElementById ('triSerie');
-    var barreRecherche = document.getElementById ('rechercheBarre');
-    var carte = document.querySelectorAll('.card');
-    const tabCartes = [];
-
-
-    // Ajouter chaque carte au tableau
-    carte.forEach(card => {
-        tabCartes.push(card);
-console.log(tabCartes)
-    });
-
-    // Extraire le texte de l'auteur de chaque carte
-    let auteurs = tabCartes.map(card => {
-        return card.querySelector('#idAuteur').textContent;
-    });
-
-    // Trier le tableau d'auteurs
-    auteurs.sort((a, b) => {
-        if (a < b) {
-            return 1;
-        } else if (a > b) {
-            return -1;
-        } else {
-            return 0;
+    // Tri par auteur(s)
+    document.getElementById('triAuteur').addEventListener('change', function() {
+        if (this.checked) {
+            var container = document.querySelector('.card-container');
+            var cards = Array.from(container.getElementsByClassName('card'));
+            cards.sort(function(a, b) {
+                var auteurA = a.querySelector('#idAuteur').textContent.toUpperCase();
+                var auteurB = b.querySelector('#idAuteur').textContent.toUpperCase();
+                if (auteurA < auteurB) {
+                    return -1;
+                }
+                if (auteurA > auteurB) {
+                    return 1;
+                }
+                return 0;
+            });
+            var row;
+            cards.forEach(function(card, index) {
+                if (index % 3 === 0) {
+                    row = document.createElement('div');
+                    row.className = 'row row-cols-1 row-cols-md-3 g-4';
+                    container.appendChild(row);
+                }
+                row.appendChild(card);
+            });
         }
     });
-
-    // Trier le tableau tabCartes en fonction des auteurs
-    tabCartes.sort((a, b) => {
-        let auteurA = a.querySelector('#idAuteur').textContent;
-        let auteurB = b.querySelector('#idAuteur').textContent;
-
-        if (auteurA < auteurB) {
-            return -1;
-        } else if (auteurA > auteurB) {
-            return 1;
-        } else {
-            return 0;
-        }
-    });
-
-    // Obtenir l'élément parent des cartes
-    let parent = document.querySelector('card-container');
-
-    // Ajouter chaque carte du tableau trié à l'élément parent
-    tabCartes.forEach(card => {
-        parent.appendChild(card);
-    });
-
-    // // Tri par auteur(s)
-    // if (triAut.checked) {
-    
-    //     // Trier les cartes en fonction du titre
-    //     tabCartes.sort((a, b) => {
-    //         let auteur1 = a.getElementById('idAuteur').textContent.toUpperCase();
-    //         let auteur2 = b.getElementById('idAuteur').textContent.toUpperCase();
-    //         if (auteur1 < auteur2) {
-    //         return -1;
-    //         } else if (auteur1 > auteur2) {
-    //         return 1;
-    //         } else {
-    //         return 0;
-    //         }
-    //     });
-
-        // // Ajouter les cartes triées à la page HTML
-        // const cardContainer = document.querySelector('.card-container');
-        // tabCartes.forEach(card => {
-        // cardContainer.appendChild(card);
-        // });
-    // }
-
 
     // Tri par série(s)
-
-
-    // Recherche
+    document.getElementById('triSerie').addEventListener('change', function() {
+        if (this.checked) {
+            var container = document.querySelector('.card-container');
+            var cards = Array.from(container.getElementsByClassName('card'));
+            cards.sort(function(a, b) {
+                var serieA = a.querySelector('#triSerie').textContent.toUpperCase();
+                var serieB = b.querySelector('#triSerie').textContent.toUpperCase();
+                if (serieA < serieB) {
+                    return -1;
+                }
+                if (serieA > serieB) {
+                    return 1;
+                }
+                return 0;
+            });
+            var row;
+            cards.forEach(function(card, index) {
+                if (index % 3 === 0) {
+                    row = document.createElement('div');
+                    row.className = 'row row-cols-1 row-cols-md-3 g-4';
+                    container.appendChild(row);
+                }
+                row.appendChild(card);
+            });
+        }
+    });
